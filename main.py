5fab9173ae35079dd6a915df599e415a0b656ad1
khalid-Ibne-Hussain <khalidtuhinbd@gmail.com>
2026-02-22 01:42:29 +0600

    upload the model and necessary files

---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# Load model
+model = tf.keras.models.load_model("plant_disease_model.keras")
+
+# Class names
+class_names = [
+ 'Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy',
+ 'Blueberry___healthy','Cherry_(including_sour)___Powdery_mildew','Cherry_(including_sour)___healthy',
+ 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust_',
+ 'Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)___healthy','Grape___Black_rot',
+ 'Grape___Esca_(Black_Measles)','Grape___Leaf_blight_(Isariopsis_Leaf_Spot)','Grape___healthy',
+ 'Orange___Haunglongbing_(Citrus_greening)','Peach___Bacterial_spot','Peach___healthy',
+ 'Pepper,_bell___Bacterial_spot','Pepper,_bell___healthy','Potato___Early_blight',
+ 'Potato___Late_blight','Potato___healthy','Raspberry___healthy','Soybean___healthy',
+ 'Squash___Powdery_mildew','Strawberry___Leaf_scorch','Strawberry___healthy','Tomato___Bacterial_spot',
+ 'Tomato___Early_blight','Tomato___Late_blight','Tomato___Leaf_Mold','Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites Two-spotted_spider_mite','Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus','Tomato___Tomato_mosaic_virus','Tomato___healthy'
+]
+
+st.title("Plant Disease Detection")
+
+uploaded_file = st.file_uploader("Upload a leaf image", type=["jpg","png","jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img = image.resize((128,128))
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    index = np.argmax(prediction)
+    confidence = np.max(prediction)
+
+    st.write("Prediction:", class_names[index])
+    st.write("Confidence:", round(float(confidence)*100,2), "%")
